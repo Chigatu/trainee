91e8fbcbbd690db73f61098b33f3261c5b2b543a
Chigatuli <137785396+Chigatu@users.noreply.github.com>
2023-08-30 16:04:48 +0300

    docker connection

--- cmd/main.go
+++ cmd/main.go
@@ -6,17 +6,27 @@ import (
 	"github.com/chigatu/service/pkg/handler"
 	"github.com/chigatu/service/pkg/repository"
 	"github.com/chigatu/service/pkg/service"
-	"guthub.com/chigatu/service/server"
+	"github.com/chigatu/service/server"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %v", err.Error())
+	}
 
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(server.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString(`port`), handlers.InitRoutes()); err != nil {
 		log.Fatal("error occured while running http server", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 .PHONY: build 
 build:
-	go build -v ./cmd/apiserver
+	go build -v ./cmd/main.go
 
 .DEFAULT_GOAL := build 
